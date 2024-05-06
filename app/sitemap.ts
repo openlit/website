a6d2351bd494cbe8c6a7c27ca5473f11a9940ff0
Aman Agarwal <aman@appsmith.com>
2024-05-06 19:19:24 +0530

    fix: removed blogs from sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,17 +5,19 @@ import siteMetadata from '@/data/siteMetadata'
 export default function sitemap(): MetadataRoute.Sitemap {
   const siteUrl = siteMetadata.siteUrl
 
-  const blogRoutes = allBlogs
-    .filter((post) => !post.draft)
-    .map((post) => ({
-      url: `${siteUrl}/${post.path}`,
-      lastModified: post.lastmod || post.date,
-    }))
+  // const blogRoutes = allBlogs
+  //   .filter((post) => !post.draft)
+  //   .map((post) => ({
+  //     url: `${siteUrl}/${post.path}`,
+  //     lastModified: post.lastmod || post.date,
+  //   }))
 
-  const routes = ['', 'blog'].map((route) => ({
+  // const routes = ['', 'blog'].map((route) => ({
+  const routes = [''].map((route) => ({
     url: `${siteUrl}/${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }))
 
-  return [...routes, ...blogRoutes]
+  // return [...routes, ...blogRoutes]
+  return [...routes]
 }
