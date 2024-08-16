aa1c45227703d8d6156c215fc73e7f4eda13de22
Aman Agarwal <aman@appsmith.com>
2024-08-16 09:11:39 +0530

    fix: remove unnecessary files

--- data-2/headerNavLinks.js
+++
@@ -1,6 +0,0 @@
-module.exports = [
-  // { href: '/', title: 'Home' },
-  // { href: '/#previews', title: 'Previews' },
-  // { href: '/#features', title: 'Features' },
-  // { href: '/#about', title: 'About' },
-]
