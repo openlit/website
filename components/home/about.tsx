272d179032e92034bed0f24337929fd719a947e1
Aman Agarwal <aman@appsmith.com>
2024-11-12 18:46:51 +0530

    fix: website

--- components/home/about.tsx
+++ components/home/about.tsx
@@ -1,14 +1,12 @@
 export default function About() {
   return (
-    <section id="about">
-      <div className="bg-muted/50 rounded-lg">
-        <div className="flex flex-col-reverse gap-8 bg-neutral-100 px-20 pb-8 dark:border-neutral-300 dark:bg-neutral-950">
-          {/* eslint-disable jsx-a11y/media-has-caption */}
-          <video preload="none">
-            <source src="/static/images/demo.mp4" type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
-        </div>
+    <section id="about" className="flex w-full">
+      <div className="flex w-full flex-col-reverse gap-8 bg-neutral-100 p-20 dark:border-neutral-300 dark:bg-neutral-950">
+        {/* eslint-disable jsx-a11y/media-has-caption */}
+        <video controls>
+          <source src="/static/images/demo.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
       </div>
     </section>
   )
